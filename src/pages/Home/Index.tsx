856f4982f829524b3bdf9855f57338e225ecd110
Lucas Pedrozo <161781256+lucas-pedrozo@users.noreply.github.com>
2025-02-26 11:23:31 -0300

    teste na home

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,18 +1,48 @@
 import "./index.css";
 import Containers from "../../components/Containers";
 import imagemSound from "../../assets/imagemsound.webp";
+import imagemTimeLine from "../../assets/imagemTimeLine.png";
+import bannerAnimado1 from "../../assets/bannerAnimado1.mp4";
 
 function Home() {
-  const onClickChange = () => {};
+  const onClickChange = () => {
+    alert("Hello World");
+  };
   return (
     <>
       <div className="home">
-        <Containers
-          img={imagemSound}
-          text="Come and discover who we are and what our work is"
-          onClick={onClickChange}
-          textButton="More"
-        />
+        <div className="newsFlex">
+          <div className="newsLeft">
+            <Containers
+              img={imagemTimeLine}
+              text="Come and discover who we are and what our work is"
+              onClick={onClickChange}
+              textButton="More"
+            />
+            <video
+              src={bannerAnimado1}
+              autoPlay
+              loop
+              muted
+              className="video"
+            ></video>
+          </div>
+          <div className="newsRight">
+            <video
+              src={bannerAnimado1}
+              autoPlay
+              loop
+              muted
+              className="video"
+            ></video>
+            <Containers
+              img={imagemSound}
+              text="Don't just focus on the visuals, the audio is more important"
+              onClick={onClickChange}
+              textButton="Sound"
+            />
+          </div>
+        </div>
       </div>
     </>
   );
