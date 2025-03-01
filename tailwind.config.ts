5af83122c1959cbf5ae6ad76889e1186949539f5
Lucas Pedrozo <161781256+lucas-pedrozo@users.noreply.github.com>
2025-03-01 12:01:50 -0300

    sem animacao

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,9 @@
 import { Config } from "tailwindcss";
-import tailwindcssAnimate from "tailwindcss-animate";
 
 const config: Config = {
   theme: {
     extend: {},
   },
-  plugins: [tailwindcssAnimate],
 };
 
 export default config;
