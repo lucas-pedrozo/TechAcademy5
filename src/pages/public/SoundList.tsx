062b5ec233a133682d39f9cb8d60e87e36650a5e
Leonardo Nunes Decaris <leonardodecaris8040@gmail.com>
2025-03-02 00:06:29 -0300

    Testes na ScroolBar

--- src/pages/public/SoundList.tsx
+++ src/pages/public/SoundList.tsx
@@ -1,12 +1,11 @@
 import FilterSounds from "../../components/ui/FilterSounds";
-import Card from "../components/Card";
 
 function SoundList() {
   return (
     <>
-      <main className="max-w-[1220px] px-[10px] py-[40px] m-auto ">
-        <div className="Card bg-[rgba(255,255,255,0.1)] backdrop-blur-xl border-[1px] border-white p-[15px] rounded-[20px] shadow-[0px_2px_10px_rgba(0,0,0,0.6)]">
-          <div className="flex ">
+      <main className="max-w-[1220px] px-[10px] py-[40px] m-auto overflow-hidden">
+        <div className="Card bg-[rgba(255,255,255,0.1)] backdrop-blur-xl border-[1px] border-white p-[15px] rounded-[20px] shadow-[0px_2px_10px_rgba(0,0,0,0.6)] h-[670px]  overflow-hidden custom-scrollbar">
+          <div className="flex overflow-y-scroll">
             <FilterSounds></FilterSounds>
 
             <h1>Sound List</h1>
