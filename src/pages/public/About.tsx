71c30a9ffa33c849804acfd7271ce534a49a2320
Lucas Pedrozo <161781256+lucas-pedrozo@users.noreply.github.com>
2025-03-09 20:23:13 -0300

    inicio do display about

--- src/pages/public/About.tsx
+++ src/pages/public/About.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 export default function About() {
     return (
-        <><h1 className="text-center">
-            hellohellohello
-        </h1>
+        <>
+            <div className="max-w-[1220px] py-[60px] px-[10px] m-auto">
+                <h1 className='font-bold text-[2.5rem] text-center'>location About</h1>
+            </div>
         </>
     )
 }
